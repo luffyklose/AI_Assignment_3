9fe6d9ed9adb3ade91efd79cbbd111dbc2622f63
Zihan Xu <luffyklose@gmail.com>
2020-07-18 15:17:25 -0400

    Add project files.

---
+++ src/PlayScene.cpp
@@ -0,0 +1,152 @@
+#include "PlayScene.h"
+#include "Game.h"
+#include "EventManager.h"
+#include "Util.h"
+
+PlayScene::PlayScene()
+{
+	PlayScene::start();
+}
+
+PlayScene::~PlayScene()
+= default;
+
+void PlayScene::draw()
+{
+	drawDisplayList();
+
+	if(m_isDebugMode)
+	{
+		Util::DrawLine(m_pPlayer->getTransform()->position, m_pPlaneSprite->getTransform()->position);
+	}
+	
+}
+
+void PlayScene::update()
+{
+	updateDisplayList();
+
+	CollisionManager::LOSCheck(m_pPlayer, m_pPlaneSprite, m_pObstacle);
+}
+
+void PlayScene::clean()
+{
+	removeAllChildren();
+}
+
+void PlayScene::handleEvents()
+{
+	EventManager::Instance().update();
+
+	// handle player movement with GameController
+	if (SDL_NumJoysticks() > 0)
+	{
+		if (EventManager::Instance().getGameController(0) != nullptr)
+		{
+			const auto deadZone = 10000;
+			if (EventManager::Instance().getGameController(0)->LEFT_STICK_X > deadZone)
+			{
+				m_pPlayer->setAnimationState(PLAYER_RUN_RIGHT);
+				m_playerFacingRight = true;
+
+				m_pPlayer->getRigidBody()->velocity = glm::vec2(5.0f, 0.0f);
+				m_pPlayer->getTransform()->position += m_pPlayer->getRigidBody()->velocity;
+				m_pPlayer->getRigidBody()->velocity *= m_pPlayer->getRigidBody()->velocity * 0.9f;
+			}
+			else if (EventManager::Instance().getGameController(0)->LEFT_STICK_X < -deadZone)
+			{
+				m_pPlayer->setAnimationState(PLAYER_RUN_LEFT);
+				m_playerFacingRight = false;
+
+				m_pPlayer->getRigidBody()->velocity = glm::vec2(-5.0f, 0.0f);
+				m_pPlayer->getTransform()->position += m_pPlayer->getRigidBody()->velocity;
+				m_pPlayer->getRigidBody()->velocity *= m_pPlayer->getRigidBody()->velocity * 0.9f;
+			}
+			else
+			{
+				if (m_playerFacingRight)
+				{
+					m_pPlayer->setAnimationState(PLAYER_IDLE_RIGHT);
+				}
+				else
+				{
+					m_pPlayer->setAnimationState(PLAYER_IDLE_LEFT);
+				}
+			}
+		}
+	}
+
+
+	// handle player movement if no Game Controllers found
+	if (SDL_NumJoysticks() < 1)
+	{
+		if (EventManager::Instance().isKeyDown(SDL_SCANCODE_A))
+		{
+			m_pPlayer->setAnimationState(PLAYER_RUN_LEFT);
+			m_playerFacingRight = false;
+
+			m_pPlayer->getRigidBody()->velocity = glm::vec2(-5.0f, 0.0f);
+			m_pPlayer->getTransform()->position += m_pPlayer->getRigidBody()->velocity;
+			m_pPlayer->getRigidBody()->velocity *= m_pPlayer->getRigidBody()->velocity * 0.9f;
+		}
+		else if (EventManager::Instance().isKeyDown(SDL_SCANCODE_D))
+		{
+			m_pPlayer->setAnimationState(PLAYER_RUN_RIGHT);
+			m_playerFacingRight = true;
+
+			m_pPlayer->getRigidBody()->velocity = glm::vec2(5.0f, 0.0f);
+			m_pPlayer->getTransform()->position += m_pPlayer->getRigidBody()->velocity;
+			m_pPlayer->getRigidBody()->velocity *= m_pPlayer->getRigidBody()->velocity * 0.9f;
+		}
+		else
+		{
+			if (m_playerFacingRight)
+			{
+				m_pPlayer->setAnimationState(PLAYER_IDLE_RIGHT);
+			}
+			else
+			{
+				m_pPlayer->setAnimationState(PLAYER_IDLE_LEFT);
+			}
+		}
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_H))
+	{
+		m_isDebugMode=!m_isDebugMode;
+	}
+	
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		TheGame::Instance()->quit();
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_1))
+	{
+		TheGame::Instance()->changeSceneState(START_SCENE);
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_2))
+	{
+		TheGame::Instance()->changeSceneState(END_SCENE);
+	}
+}
+
+void PlayScene::start()
+{
+	// Plane Sprite
+	m_pPlaneSprite = new Plane();
+	addChild(m_pPlaneSprite);
+
+	// Player Sprite
+	m_pPlayer = new Player();
+	addChild(m_pPlayer);
+	m_playerFacingRight = true;
+
+	// Obstacle Texture
+	m_pObstacle = new Obstacle();
+	addChild(m_pObstacle);
+
+	m_isDebugMode = false;	
+}
---
+++ src/PlayScene.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef __PLAY_SCENE__
+#define __PLAY_SCENE__
+
+#include "Scene.h"
+#include "Plane.h"
+#include "Player.h"
+#include "Button.h"
+#include "Obstacle.h"
+
+class PlayScene : public Scene
+{
+public:
+	PlayScene();
+	~PlayScene();
+
+	// Scene LifeCycle Functions
+	virtual void draw() override;
+	virtual void update() override;
+	virtual void clean() override;
+	virtual void handleEvents() override;
+	virtual void start() override;
+private:
+	glm::vec2 m_mousePosition;
+
+	Plane* m_pPlaneSprite;
+	Player* m_pPlayer;
+	bool m_playerFacingRight;
+	bool m_isDebugMode;
+
+	Obstacle* m_pObstacle;
+	
+};
+
+#endif /* defined (__PLAY_SCENE__) */
